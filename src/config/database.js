bdf05fefd7a373fb14fdcc11268c869045d35fc1
salim <salimbhandi07@gmail.com>
2024-08-30 16:25:36 +0530

    Updated database file

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,12 @@ const DATABASE_URI =
   "mongodb+srv://salimbhandi07:H0ku5309ydEzBxqF@cluster0.s3sgs.mongodb.net/";
 const connectDB = async () => {
   try {
-    await mongoose.connect(DATABASE_URI);
+    // await mongoose.connect(DATABASE_URI);
+    await mongoose.connect(DATABASE_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      family: 4,
+    });
     console.log("MongoDB Connected");
   } catch (err) {
     console.log("MongoDB Connection Error : ", err);
